c34cb4db0f094c5743d976a18196a69dae4f6653
bibash <bibashjaprel@gmail.com>
2021-12-24 08:40:48 +0545

    makes uppercase lowercase support

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 print('---------------You wanna play this game--------------')
 player =input('if you wanna play then enter y if you dont enter x :')
 
-if player != "y":
+if player.lower() != "y":
     quit()
 
 print("okay lets play :) ")
 
 question=input('what is the full form of cpu? : ')
-if question=="central processing unit":
+if question.lower()=="central processing unit":
      print('correct')
 else:
      print('incorrect')
 
 question=input('what is the full form of LCD? : ')
-if question==" liquid crystal display":
+if question.lower()==" liquid crystal display":
      print('correct')
 else:
      print('incorrect')
 
 
 question=input('what is the full form of ALU? : ')
-if question=="arithmetic logic unit":
+if question.lower()=="arithmetic logic unit":
      print('correct')
 else:
      print('incorrect')
 
 question=input('what is the full form of ATM? : ')
-if question=="automated teller machine":
+if question.lower()=="automated teller machine":
      print('correct')
 else:
      print('incorrect')
 
 question=input('what is the full form of GUI? : ')
-if question=="graphical user interface":
+if question.lower()=="graphical user interface":
      print('correct')
 else:
      print('incorrect')
 
 question=input('what is the full form of API? : ')
-if question=="application programming interface":
+if question.lower()=="application programming interface":
      print('correct')
 else:
      print('incorrect')
